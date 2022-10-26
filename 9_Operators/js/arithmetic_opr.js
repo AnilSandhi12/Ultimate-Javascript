5703fa6c365b3f7f4ef15acf6ee77d8866492be6
AnilSandhi12 <anilsandhi01@gmail.com>
2022-10-26 16:04:21 +0530

    first commit

---
+++ 9_Operators/js/arithmetic_opr.js
@@ -0,0 +1,15 @@
+// Arithmetic operators are used to perform arithmetic operations on the operands. The following operators are known as JavaScript arithmetic operators.
+/*
+                JavaScript Arithmetic Operators
+                Arithmetic operators are used to perform arithmetic on numbers:
+
+                Operator	                  Description
+                  +	                            Addition
+                  -    	                        Subtraction
+                  *	                            Multiplication
+                  **	                        Exponentiation (ES2016)
+                  / 	                        Division
+                  %	                            Modulus (Division Remainder)
+                  ++	                        Increment
+                  --	                        Decrement
+*/
